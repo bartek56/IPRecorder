c5b33fab1f5ee2e8ce4505f034bb93ba33ec81da
Bartosz Brzozowski <bartosz.brzozowski23@gmail.com>
2022-01-07 20:43:06 +0100

    GSMSerial class

--- GSMSerial.py
+++ GSMSerial.py
@@ -5,9 +5,92 @@ import datetime
 import urllib
 import sh
 
-ser = serial.Serial('/dev/ttyAMA0',19200)  # open serial port
 dirNameBrama = '/sharedfolders/MONITORING/brama_cam'
 dirNameAltanka = '/sharedfolders/MONITORING/altanka_cam'
+
+
+class GSMSerial:
+
+    serialGSM = serial.Serial('/dev/ttyAMA0',19200)  # open serial port   
+    #serialGSM = serial.Serial('/dev/virtualcom0',19200)  # open serial port   
+    def __init__(self):
+        print("init")
+        commend1='AT\r\n'
+        self.serialGSM.write(commend1.encode())
+        while self.serialGSM.inWaiting() <= 1:
+            time.sleep(0.01)
+        self.serialGSM.read(self.serialGSM.inWaiting())
+        time.sleep(0.5)
+
+        commend1='AT+CMGF=1\r\n'
+        self.serialGSM.write(commend1.encode())
+        while self.serialGSM.inWaiting() <= 1:
+            time.sleep(0.01)
+        self.serialGSM.read(self.serialGSM.inWaiting())
+        time.sleep(0.5)
+
+        commend2='AT+CNMI=1,2,0,0\r\n'
+        self.serialGSM.write(commend2.encode())
+        while self.serialGSM.inWaiting()<=1:
+            time.sleep(0.01)
+        self.serialGSM.read(self.serialGSM.inWaiting())
+        time.sleep(0.5)
+
+        commend3='AT+CLIP=1\r\n'
+        self.serialGSM.write(commend3.encode())
+        while self.serialGSM.inWaiting()<=1:
+            time.sleep(0.01)
+        time.sleep(0.5)
+
+    def sendSMS(self, number, text):
+        commend='AT+CMGS="' + str(number) + '"\r\n'
+        self.serialGSM.write(commend.encode())
+        while self.serialGSM.inWaiting()<=10:
+            time.sleep(0.04)
+        time.sleep(0.2)
+        data_str = self.serialGSM.read(self.serialGSM.inWaiting())
+        if ('>' in data_str):
+            self.serialGSM.write(text.encode()) #\x1a
+            self.serialGSM.write(b'\x1a')
+
+    def sendSMSAdmin(self, text):
+        self.sendSMS(999999999, text)
+
+    def sendSMSUser(self, text):
+        self.sendSMS(999999999, text)
+    
+    def readMessages(self):
+    # NB: for PySerial v3.0 or later, use property `in_waiting` instead of function `inWaiting()` below!
+        if (self.serialGSM.inWaiting()>0): #if incoming bytes are waiting to be read from the serial input buffer
+            #data_str = ser.read(ser.inWaiting()).decode('ascii') #read the bytes and convert from binary array to ASCII
+            data_str = self.serialGSM.read(self.serialGSM.inWaiting())
+            self.GSMLog(data_str)
+            if('ALARMOFF' in data_str):
+                ALARMON=False
+                self.sendSMSUser("Wylaczono ALARM")
+            elif('ALARMON' in data_str):
+                ALARMON=True
+                self.sendSMSUser("Wlaczono ALARM")
+            elif('STATUS' in data_str):
+                status = checkStatus()
+                self.sendSMSAdmin(status)
+            elif('+CLIP:' in data_str):
+                self.sendSMSAdmin("somebody call to me: \n" + data_str)
+#            elif('AT+' not in data_str):
+#                sendSMSAdmin("I don't understand \n" + data_str)
+
+
+    
+
+
+    def GSMLog(self, info):
+        date_time = datetime.datetime.now().strftime("%Y/%m/%d, %H:%M:%S")
+        answer = date_time + ": " + info + '\n'
+        file = open("/var/log/GSMSerial.log",'a')
+        file.writelines(answer)
+        file.close()
+ 
+
 
 def alarmLog(info):
     date_time = datetime.datetime.now().strftime("%Y/%m/%d, %H:%M:%S")
@@ -16,30 +99,7 @@ def alarmLog(info):
     file.writelines(answer)
     file.close()
 
-def GSMLog(info):
-    date_time = datetime.datetime.now().strftime("%Y/%m/%d, %H:%M:%S")
-    answer = date_time + ": " + info + '\n'
-    file = open("/var/log/GSMSerial.log",'a')
-    file.writelines(answer)
-    file.close()
-    
-
-def sendSMS(number, text):
-    commend='AT+CMGS="' + str(number) + '"\r\n'
-    ser.write(commend.encode())
-    while ser.inWaiting()<=10:
-        time.sleep(0.04)
-    time.sleep(0.2)
-    data_str = ser.read(ser.inWaiting())
-    if ('>' in data_str):
-        ser.write(text.encode()) #\x1a
-        ser.write(b'\x1a')
-
-def sendSMSAdmin(text):
-    sendSMS(999999999, text)
-
-def sendSMSUser(text):
-    sendSMS(999999999, text)
+   
 
 def getTheNewestDayDir(dirName):
     if os.path.isdir(dirName):
@@ -48,9 +108,8 @@ def getTheNewestDayDir(dirName):
         latest_dir=max(dirs, key=os.path.basename)
         return latest_dir
     else:
-        GSMLog("error with Disk")
-        sendSMSAdmin("Error with Disk")
-        exit()
+        return 0
+
 
 #    sub_dirs = [d for d in os.listdir('/sharedfolders/MONITORING/brama_cam/'+latest_dir+'/001/jpg/')]
 #    latest_dir_hours=max(sub_dirs, key=os.path.basename)
@@ -63,38 +122,8 @@ def getListOfFiles(dirName):
             listOfFiles += [os.path.join(dirpath, f) for f in os.listdir(dirpath) if f.endswith('.jpg')]
         return len(listOfFiles)
     else:
-        GSMLog("error with Disk")
-        sendSMSAdmin("Error with Disk")
-        exit()
+        return 0
 
-
-def initGSM():
-    commend1='AT\r\n'
-    ser.write(commend1.encode())
-    while ser.inWaiting()<=1:
-        time.sleep(0.01)
-    ser.read(ser.inWaiting())
-    time.sleep(0.5)
-
-    commend1='AT+CMGF=1\r\n'
-    ser.write(commend1.encode())
-    while ser.inWaiting()<=1:
-        time.sleep(0.01)
-    ser.read(ser.inWaiting())
-    time.sleep(0.5)
-
-    commend2='AT+CNMI=1,2,0,0\r\n'
-    ser.write(commend2.encode())
-    while ser.inWaiting()<=1:
-        time.sleep(0.01)
-    ser.read(ser.inWaiting())
-    time.sleep(0.5)
-
-    commend3='AT+CLIP=1\r\n'
-    ser.write(commend3.encode())
-    while ser.inWaiting()<=1:
-        time.sleep(0.01)
-    time.sleep(0.5)
 
 def checkNetwork():
     try:
@@ -123,12 +152,11 @@ def checkStatus():
         return "everything okay \n" + checkMemory()
     else:
         return info
-    
+
 
 def main():
     ALARMON = False
-    time.sleep(3)
-    initGSM()
+    gsm =  GSMSerial()
     time.sleep(3)
     
     counter=0
@@ -137,44 +165,40 @@ def main():
     countFilesBrama=0
     countFilesAltanka=0
     theNewestDirAltanka = getTheNewestDayDir(dirNameAltanka)
+    if(theNewestDirAltanka == 0): 
+        gsm.sendSMSAdmin("Error with Disk")
+        exit()
+
     theNewestDirBrama = getTheNewestDayDir(dirNameBrama)
     countFilesBrama = getListOfFiles(dirNameBrama+'/'+theNewestDirBrama)
     countFilesAltanka = getListOfFiles(dirNameAltanka+'/'+theNewestDirAltanka)
+    
     while (True):
-    # NB: for PySerial v3.0 or later, use property `in_waiting` instead of function `inWaiting()` below!
-        if (ser.inWaiting()>0): #if incoming bytes are waiting to be read from the serial input buffer
-            #data_str = ser.read(ser.inWaiting()).decode('ascii') #read the bytes and convert from binary array to ASCII
-            data_str = ser.read(ser.inWaiting())
-            GSMLog(data_str)
-            if('ALARMOFF' in data_str):
-                ALARMON=False
-                sendSMSUser("Wylaczono ALARM")
-            elif('ALARMON' in data_str):
-                ALARMON=True
-                sendSMSUser("Wlaczono ALARM")
-            elif('STATUS' in data_str):
-                status = checkStatus()
-                sendSMSAdmin(status)
-            elif('+CLIP:' in data_str):
-                sendSMSAdmin("somebody call to me: \n" + data_str)
-#            elif('AT+' not in data_str):
-#                sendSMSAdmin("I don't understand \n" + data_str)
-
 
         if (counter > 20):
             counter=0
             newTheNewestDirAltanka = getTheNewestDayDir(dirNameAltanka)
+            if(newTheNewestDirAltanka == 0):
+                gsm.sendSMSAdmin("Error with Disk")
+                exit()
+
             if (newTheNewestDirAltanka!= theNewestDirAltanka):  #new directory -> new day
                 countFilesAltanka = 0
                 theNewestDirAltanka=newTheNewestDirAltanka
             newCountFilesAltanka = getListOfFiles(dirNameAltanka+'/'+theNewestDirAltanka)
+            if(newCountFilesAltanka == 0):
+                gsm.sendSMSAdmin("Error with Disk")
+                exit()
+
+
+
             if(newCountFilesAltanka-countFilesAltanka==2): # come in to loop, when somebody come in to possesion
                 time.sleep(1.5)
 
             if(newCountFilesAltanka-countFilesAltanka>2):
                 info = "ALARM ALTANKA"
                 if(ALARMON==True):
-                    sendSMSUser(info)
+                    gsm.sendSMSUser(info)
                 alarmLog(info)
             countFilesAltanka=newCountFilesAltanka
 
@@ -187,7 +211,7 @@ def main():
             if(newCountFilesBrama-countFilesBrama>2):
                 info = "ALARM BRAMA"
                 if(ALARMON==True):
-                    sendSMSUser(info)
+                    gsm.sendSMSUser(info)
                 alarmLog(info)
             countFilesBrama=newCountFilesBrama
         
@@ -197,7 +221,7 @@ def main():
             file = open("/etc/scripts/SMS.txt","r")
             text = file.read()
             file.close()
-            sendSMSAdmin(text)
+            gsm.sendSMSAdmin(text)
             os.remove("/etc/scripts/SMS.txt")
         
         time.sleep(0.5)
