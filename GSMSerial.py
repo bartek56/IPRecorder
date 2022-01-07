be48722951692c489dd0770fd9fc542e642609a6
Bartosz Brzozowski <bartosz.brzozowski23@gmail.com>
2022-01-07 22:07:58 +0100

    README.md; alarm class

--- GSMSerial.py
+++ GSMSerial.py
@@ -11,10 +11,44 @@ dirNameAltanka = '/sharedfolders/MONITORING/altanka_cam'
 ADMIN_NUMBER=999999999
 USER_NUMBER=999999999
 
-class GSMSerial:
 
-    serialGSM = serial.Serial('/dev/ttyAMA0',19200)  # open serial port   
-    #serialGSM = serial.Serial('/dev/virtualcom0',19200)  # open serial port   
+class IpRecorderStatus:
+    def __init__(self) -> None:
+        pass
+
+    def checkNetwork(self):
+        try:
+            urllib.urlopen("http://google.com") #Python 3.x
+            return True
+        except:
+            return False
+
+    def checkMemory(self):
+        rootMemory = sh.awk( sh.grep(sh.df("-h"), "root"), "{print $3 \"/\" $2}")
+        diskMemory = sh.awk( sh.grep(sh.df("-h"), "INTENSO"), "{print $3 \"/\" $2}")
+        rootMemory = str(rootMemory).strip()
+        diskMemory = str(diskMemory).strip()
+        return ("External Memory: " + diskMemory + "\n" + "Internal Memory: " + rootMemory)
+
+    def checkStatus(self):
+        info = ""
+        if not os.path.isdir(dirNameBrama):
+            info += "disk is not mounted"
+        if not self.checkNetwork():
+            if len(info) > 3:
+                info += "\n"
+            info += "I'm not connected to network \n" + self.checkMemory()
+    
+        if len(info) < 3:
+            return "everything okay \n" + self.checkMemory()
+        else:
+            return info
+
+
+class GSMSerial:
+    ipRecorderStatus = IpRecorderStatus()
+    #serialGSM = serial.Serial('/dev/ttyAMA0',19200)  # open serial port   
+    serialGSM = serial.Serial('/dev/virtualcom0',19200)  # open serial port   
     def __init__(self):
         print("init")
         commend1='AT\r\n'
@@ -47,7 +81,7 @@ class GSMSerial:
     def sendSMS(self, number, text):
         commend='AT+CMGS="' + str(number) + '"\r\n'
         self.serialGSM.write(commend.encode())
-        while self.serialGSM.inWaiting()<=10:
+        while self.serialGSM.inWaiting()<=1:
             time.sleep(0.04)
         time.sleep(0.2)
         data_str = self.serialGSM.read(self.serialGSM.inWaiting())
@@ -62,8 +96,8 @@ class GSMSerial:
         self.sendSMS(USER_NUMBER, text)
     
     def readMessages(self):
-    # NB: for PySerial v3.0 or later, use property `in_waiting` instead of function `inWaiting()` below!
         if (self.serialGSM.inWaiting()>0): #if incoming bytes are waiting to be read from the serial input buffer
+            # NB: for PySerial v3.0 or later, use property `in_waiting` instead of function `inWaiting()` below!
             #data_str = ser.read(ser.inWaiting()).decode('ascii') #read the bytes and convert from binary array to ASCII
             data_str = self.serialGSM.read(self.serialGSM.inWaiting())
             self.GSMLog(data_str)
@@ -74,13 +108,12 @@ class GSMSerial:
                 ALARMON=True
                 self.sendSMSUser("Wlaczono ALARM")
             elif('STATUS' in data_str):
-                status = checkStatus()
+                status = self.ipRecorderStatus.checkStatus()
                 self.sendSMSAdmin(status)
             elif('+CLIP:' in data_str):
                 self.sendSMSAdmin("somebody call to me: \n" + data_str)
-#            elif('AT+' not in data_str):
-#                sendSMSAdmin("I don't understand \n" + data_str)
-
+                #elif('AT+' not in data_str):
+                #sendSMSAdmin("I don't understand \n" + data_str)
 
     def GSMLog(self, info):
         date_time = datetime.datetime.now().strftime("%Y/%m/%d, %H:%M:%S")
@@ -90,93 +123,60 @@ class GSMSerial:
         file.close()
  
 
+class Alarm:
+    def __init__(self):
+        pass
 
-def alarmLog(info):
-    date_time = datetime.datetime.now().strftime("%Y/%m/%d, %H:%M:%S")
-    answer = date_time + ": " + info + '\n'
-    file = open("/var/log/Alarm.log",'a')
-    file.writelines(answer)
-    file.close()
+    def getTheNewestDayDir(self, dirName):
+        if os.path.isdir(dirName):
+            dirs = [d for d in os.listdir(dirName)]
+            dirs.remove('DVRWorkDirectory')
+            latest_dir=max(dirs, key=os.path.basename)
+            return latest_dir
+        else:
+            return 0
 
-   
+    def getListOfFiles(dirName):
+        if os.path.isdir(dirName):
+            listOfFiles = list()
+            for (dirpath, dirnames, filenames) in os.walk(dirName):
+                listOfFiles += [os.path.join(dirpath, f) for f in os.listdir(dirpath) if f.endswith('.jpg')]
+            return len(listOfFiles)
+        else:
+            return 0
 
-def getTheNewestDayDir(dirName):
-    if os.path.isdir(dirName):
-        dirs = [d for d in os.listdir(dirName)]
-        dirs.remove('DVRWorkDirectory')
-        latest_dir=max(dirs, key=os.path.basename)
-        return latest_dir
-    else:
-        return 0
-
-
-#    sub_dirs = [d for d in os.listdir('/sharedfolders/MONITORING/brama_cam/'+latest_dir+'/001/jpg/')]
-#    latest_dir_hours=max(sub_dirs, key=os.path.basename)
-#    print latest_dir_ho
-
-def getListOfFiles(dirName):
-    if os.path.isdir(dirName):
-        listOfFiles = list()
-        for (dirpath, dirnames, filenames) in os.walk(dirName):
-            listOfFiles += [os.path.join(dirpath, f) for f in os.listdir(dirpath) if f.endswith('.jpg')]
-        return len(listOfFiles)
-    else:
-        return 0
-
-
-def checkNetwork():
-    try:
-        urllib.urlopen("http://google.com") #Python 3.x
-        return True
-    except:
-        return False
-
-def checkMemory():
-    rootMemory = sh.awk( sh.grep(sh.df("-h"), "root"), "{print $3 \"/\" $2}")
-    diskMemory = sh.awk( sh.grep(sh.df("-h"), "INTENSO"), "{print $3 \"/\" $2}")
-    rootMemory = str(rootMemory).strip()
-    diskMemory = str(diskMemory).strip()
-    return ("External Memory: " + diskMemory + "\n" + "Internal Memory: " + rootMemory)
-
-def checkStatus():
-    info = ""
-    if not os.path.isdir(dirNameBrama):
-        info += "disk is not mounted"
-    if not checkNetwork():
-        if len(info) > 3:
-            info += "\n"
-        info += "I'm not connected to network \n" + checkMemory()
-    
-    if len(info) < 3:
-        return "everything okay \n" + checkMemory()
-    else:
-        return info
+    def alarmLog(self, info):
+        date_time = datetime.datetime.now().strftime("%Y/%m/%d, %H:%M:%S")
+        answer = date_time + ": " + info + '\n'
+        file = open("/var/log/Alarm.log",'a')
+        file.writelines(answer)
+        file.close()
 
 
 def main():
     ALARMON = False
-    gsm =  GSMSerial()
+    gsm = GSMSerial()
     time.sleep(3)
-    
+    alarm = Alarm()
     counter=0
     counterAltanka=0
     counterBrama=0
     countFilesBrama=0
     countFilesAltanka=0
-    theNewestDirAltanka = getTheNewestDayDir(dirNameAltanka)
+    theNewestDirAltanka = alarm.getTheNewestDayDir(dirNameAltanka)
     if(theNewestDirAltanka == 0): 
         gsm.sendSMSAdmin("Error with Disk")
         exit()
 
-    theNewestDirBrama = getTheNewestDayDir(dirNameBrama)
-    countFilesBrama = getListOfFiles(dirNameBrama+'/'+theNewestDirBrama)
-    countFilesAltanka = getListOfFiles(dirNameAltanka+'/'+theNewestDirAltanka)
-    
+    theNewestDirBrama = alarm.getTheNewestDayDir(dirNameBrama)
+    countFilesBrama = alarm.getListOfFiles(dirNameBrama+'/'+theNewestDirBrama)
+    countFilesAltanka = alarm.getListOfFiles(dirNameAltanka+'/'+theNewestDirAltanka)
+    print("Ready")
     while (True):
         gsm.readMessages()
         if (counter > 20):
             counter=0
-            newTheNewestDirAltanka = getTheNewestDayDir(dirNameAltanka)
+            newTheNewestDirAltanka = alarm.getTheNewestDayDir(dirNameAltanka)
             if(newTheNewestDirAltanka == 0):
                 gsm.sendSMSAdmin("Error with Disk")
                 exit()
@@ -184,12 +184,10 @@ def main():
             if (newTheNewestDirAltanka!= theNewestDirAltanka):  #new directory -> new day
                 countFilesAltanka = 0
                 theNewestDirAltanka=newTheNewestDirAltanka
-            newCountFilesAltanka = getListOfFiles(dirNameAltanka+'/'+theNewestDirAltanka)
+            newCountFilesAltanka = alarm.getListOfFiles(dirNameAltanka+'/'+theNewestDirAltanka)
             if(newCountFilesAltanka == 0):
                 gsm.sendSMSAdmin("Error with Disk")
                 exit()
-
-
 
             if(newCountFilesAltanka-countFilesAltanka==2): # come in to loop, when somebody come in to possesion
                 time.sleep(1.5)
@@ -198,20 +196,20 @@ def main():
                 info = "ALARM ALTANKA"
                 if(ALARMON==True):
                     gsm.sendSMSUser(info)
-                alarmLog(info)
+                alarm.alarmLog(info)
             countFilesAltanka=newCountFilesAltanka
 
-            newTheNewestDirBrama = getTheNewestDayDir(dirNameBrama)
+            newTheNewestDirBrama = alarm.getTheNewestDayDir(dirNameBrama)
             if (newTheNewestDirBrama != theNewestDirBrama):
                 countFilesBrama = 0
                 theNewestDirBrama=newTheNewestDirBrama
-            newCountFilesBrama = getListOfFiles(dirNameBrama+'/'+theNewestDirBrama)
+            newCountFilesBrama = alarm.getListOfFiles(dirNameBrama+'/'+theNewestDirBrama)
 
             if(newCountFilesBrama-countFilesBrama>2):
                 info = "ALARM BRAMA"
                 if(ALARMON==True):
                     gsm.sendSMSUser(info)
-                alarmLog(info)
+                alarm.alarmLog(info)
             countFilesBrama=newCountFilesBrama
         
         counter=counter+1
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # IpRecorder
 scripts for IpRecorder (OpenMediaVault)
+
+GSMSerial.py:
+It's script to verify new image (created by IP Camera during moving) and inform by SMS about it
+You can test it with [AT simulator](https://github.com/celersms/AT-Emulator) on Ubuntu 
+
+GSMSerial.service
+systemd service to auto start GSMSerial.py
+
+autoRemove.sh
+script to remove videos/images after 10 days
+
+clearLogs.sh, clearSysLogs.sh
+clear contains in log files
+
+
+
