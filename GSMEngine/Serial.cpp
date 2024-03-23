5023fba6bbfe5b81a5905cb9c3f31b1f4cc30eae
iprecorder <bartosz.brzozowski23@gmail.com>
2024-03-23 22:13:58 +0100

    configure serial for GSM module

--- GSMEngine/Serial.cpp
+++ GSMEngine/Serial.cpp
@@ -10,73 +10,71 @@ Serial::Serial() { std::cout << "ctor" << std::endl; }
 
 void Serial::start() {
   std::cout << "start" << std::endl;
-  // Ścieżka do urządzenia UART (np. /dev/ttyS0, /dev/ttyUSB0 itp.)
-  const char *uartDevice = "/dev/pts/2";
+//  const char *uartDevice = "/dev/pts/2";
+  const char *uartDevice = "/dev/ttyAMA0";
 
-  // Otwarcie urządzenia UART
   int fd = open(uartDevice, O_RDWR | O_NOCTTY | O_NDELAY);
   if (fd == -1) {
     std::cerr << "Nie można otworzyć urządzenia UART." << std::endl;
     return;
   }
 
-  // Konfiguracja parametrów portu szeregowego
+  // serial port configuration
   struct termios options;
   tcgetattr(fd, &options);
-  cfsetispeed(&options, B9600);
-  cfsetospeed(&options, B9600);
+  cfsetispeed(&options, B19200);
+  cfsetospeed(&options, B19200);
   options.c_cflag |= (CLOCAL | CREAD);
-  options.c_cflag &= ~PARENB;
-  options.c_cflag &= ~CSTOPB;
-  options.c_cflag &= ~CSIZE;
-  options.c_cflag |= CS8;
+  options.c_cflag &= ~PARENB; // disable parity
+  options.c_cflag &= ~CSTOPB; // one bit of stop
+  options.c_cflag &= ~CSIZE; // disable bits of size
+  options.c_cflag |= CS8; // 8 bits of data
   options.c_lflag = 0; //~(ICANON | ECHO | ECHOE | ISIG);
   options.c_iflag = 0; // ~(IXON | IXOFF | IXANY);
   tcsetattr(fd, TCSANOW, &options);
 
-  // Ustawienie non-blocking mode
+  // non-blocking mode
   fcntl(fd, F_SETFL, O_NONBLOCK);
 
-  // Inicjalizacja zbioru deskryptorów
   fd_set read_fds;
   FD_ZERO(&read_fds);
   FD_SET(fd, &read_fds);
 
   while (true) {
 
-    // Ustawienie czasu oczekiwania na zero
     struct timeval timeout;
     timeout.tv_sec = 2;
     timeout.tv_usec = 0;
     FD_ZERO(&read_fds);
     FD_SET(fd, &read_fds);
 
-    // Czekanie na dane przy użyciu select
+    // wait for data
     int result = select(fd + 1, &read_fds, NULL, NULL, &timeout);
     if (result == -1) {
-      std::cerr << "Błąd select()." << std::endl;
+      std::cerr << "error with select()." << std::endl;
       break;
     } else if (result == 0) {
-      std::cout << "Brak danych w danym przedziale czasu." << std::endl;
+      std::cout << "No data" << std::endl;
     } else {
 
-      // Sprawdzanie, czy zdarzenie dotyczy naszego deskryptora
+      // check if select is for out device
       if (FD_ISSET(fd, &read_fds)) {
-        // Odczytywanie danych z urządzenia UART
+        // read data
         char buffer[256];
         memset(buffer, 0, sizeof(buffer));
+				usleep(100000); // wait to get whole message from serial
         int bytesRead = read(fd, buffer, sizeof(buffer) - 1);
         if (bytesRead > 0) {
-          std::cout << "Otrzymane dane: " << buffer << std::endl;
+          std::cout << "New data: " << buffer << std::endl;
         }
       }
     }
 
-    // Czekanie na nowe zdarzenia
-    // usleep(100000); // Oczekiwanie 0.1 sekundy (opcjonalne)
+    // let's do other things
+    usleep(100000);
     std::cout << "wait" << std::endl;
   }
 
-  // Zamykanie urządzenia UART
   close(fd);
+	std::cout << "serial was closed" << std::endl;
 }
