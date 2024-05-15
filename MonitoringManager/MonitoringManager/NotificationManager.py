8cd9e8777372b9fe2e943047fa7b992ffa7446db
Bartosz Brzozowski <bartosz.brzozowski23@gmail.com>
2024-05-15 20:26:26 +0200

    update python MonitoringManager with cpp lib support

--- MonitoringManager/MonitoringManager/NotificationManager.py
+++ MonitoringManager/MonitoringManager/NotificationManager.py
@@ -2,15 +2,17 @@ import os
 import time
 
 from Contacts import Contacts
-from GSMSerial import GSMSerial, IpRecorderStatus
-#from mailManager import Mail
+from StatusManager import IpRecorderStatus
+# from mailManager import Mail
 from Logger import Logger
+import libGSMEngine as GSMSerial
 
 
 class ActiveUser():
     def __init__(self, name="", surname=""):
         self.name = name
         self.surname = surname
+
 
 class NotificationManager():
     def __init__(self, activeUsersFile="/etc/scripts/active_users.txt"):
@@ -19,7 +21,8 @@ class NotificationManager():
 #        self.mailManager = Mail()
         self.phoneContacts = Contacts("/etc/scripts/contacts.txt")
         self.readyToSMS = False
-        self.gsmSerial = GSMSerial()
+        self.gsmManager = GSMSerial.GSMManager("/dev/ttyAMA0")
+        self.gsmManager.initialize()
         time.sleep(3)
         self.readyToSMS = True
         self.usersList = []
@@ -29,13 +32,13 @@ class NotificationManager():
             lines = file.readlines()
             file.close()
             for user in lines:
-                if len(user)>2:
+                if len(user) > 2:
                     userNameAndSurname = user.strip()
                     userNameAndSurnameList = userNameAndSurname.split(" ")
                     name = userNameAndSurnameList[0]
                     surname = userNameAndSurnameList[1]
                     self.usersList.append(ActiveUser(name, surname))
-    
+
     def showActiveContacts(self):
         for x in self.usersList:
             print(x.name, x.surname)
@@ -43,14 +46,14 @@ class NotificationManager():
     def getActiveContacts(self):
         activeContact = []
         for x in self.usersList:
-            activeContact.append(x.name + " " +x.surname)
-        return activeContact    
+            activeContact.append(x.name + " " + x.surname)
+        return activeContact
 
     def addUserAsActive(self, name, surname):
         for x in self.usersList:
             if x.name == name and x.surname == surname:
                 Logger.WARNING("Contact exist in active users")
-                return 
+                return
 
         contactExist = False
         for x in self.phoneContacts.tel:
@@ -58,8 +61,8 @@ class NotificationManager():
                 contactExist = True
         if not contactExist:
             Logger.WARNING("Contact", name, surname, "not exist")
-            return 
-           
+            return
+
         self.usersList.append(ActiveUser(name, surname))
         Logger.INFO("added contact as active:", name, surname)
 
@@ -68,15 +71,16 @@ class NotificationManager():
         contactExist = False
         for x in self.usersList:
             if x.name == name and x.surname == surname:
-                contactExist = True 
+                contactExist = True
                 break
-            index+=1    
-        
-        if(contactExist):
+            index += 1
+
+        if (contactExist):
             Logger.INFO("Contact", name, surname, " removed from active list")
             self.usersList.pop(index)
         else:
-            Logger.WARNING("Contact", name, surname, "not exist in active list")
+            Logger.WARNING("Contact", name,
+                           surname, "not exist in active list")
 
     def saveToFile(self):
         fileToSave = open(self.fileName, "w")
@@ -87,25 +91,25 @@ class NotificationManager():
             fileToSave.write(user.surname)
             fileToSave.write("\n")
 
-        self.phoneContacts.SaveToFile()    
+        self.phoneContacts.SaveToFile()
 
-#    def sendMailNotification(self, subject, message):        
+#    def sendMailNotification(self, subject, message):
 #        for x in self.usersList:
 #            nameAndSurname = x.name + " " + x.surname
 #            email = self.phoneContacts.GetDefaultEmail(nameAndSurname)
 #            self.mailManager.sendMail(email, subject, message)
-    
-    def sendSMSNotification(self, message):        
+
+    def sendSMSNotification(self, message):
         for x in self.usersList:
             nameAndSurname = x.name + " " + x.surname
             number = self.phoneContacts.GetDefaultNumber(nameAndSurname)
-            self.gsmSerial.sendSMS(number, message)
+            self.gsmManager.sendSms(number, message)
 
-    def sendSMS(self, number, message):        
-        self.gsmSerial.sendSMS(number, message)
+    def sendSMS(self, number, message):
+        self.gsmManager.sendSms("+48"+number, message) # TODO
 
-    def sendSMSAdmin(self, message):        
-        self.gsmSerial.sendSMS(self.adminNumber, message)
+    def sendSMSAdmin(self, message):
+        self.gsmManager.sendSms("+48"+self.adminNumber, message) # TODO
 
     def checkStatus(self):
         status = self.ipRecorderStatus.checkStatus()
@@ -115,57 +119,36 @@ class NotificationManager():
             for x in activeContacts:
                 status += x
                 status += " "
-        return status        
+        return status
 
-
-    def checkSender(self, data_str):
-        dataStrTemp = data_str
-        dataStrTempSplitted = dataStrTemp.split(",,")
-        number = dataStrTempSplitted[0].replace('"', '')
-        number = number.replace("+48", "")
-        number = number.replace("+CMT: ", "")
-
-        number = number.strip()
+    def checkSender(self, number):
+        number = number.replace("+48", "") # TODO
         contact = self.phoneContacts.LookingForContactByNumber(number)
-
         return contact
 
+    def readAT(self):
+        if (not self.gsmManager.isNewSms()):
+            return
+        sms = self.gsmManager.getSms()
+        contact = self.checkSender(sms.number)
+        if contact is None:
+            self.sendSMSAdmin("unknown number send sms to me")
+            return
+        data_str = sms.msg
 
-    def readAT(self):    
-        data_str = self.gsmSerial.readMessages()
-
-        if(data_str is not None):
-            # text message        
-            if('+CMT:' in data_str):
-                contact = self.checkSender(data_str)
-                if contact is None:
-                     self.sendSMSAdmin("unknown number send sms to me")
-                     return
-                else:                        
-                    if('STATUS' in data_str):
-                        self.sendSMS(contact.numbers[0].number, self.checkStatus())
-                    elif('ALARMOFF' in data_str):
-                        self.sendSMS(contact.numbers[0].number, "Wylaczono ALARM")
-                        self.removeUserFromActive(contact.name, contact.surname)
-                    elif('ALARMON' in data_str):
-                        self.sendSMS(contact.numbers[0].number, "Wlaczono ALARM")
-                        self.addUserAsActive(contact.name, contact.surname)
-                    else:
-                        self.sendSMS(contact.numbers[0].number, "Nie wiem, czego ode mnie zadasz")
-
-            # calling           
-            elif('+CLIP:' in data_str):
-                self.sendSMSAdmin("somebody call to me: \n" + data_str)
-            # send SMS    
-            elif('+CMGS:' in data_str):
-                self.readyToSMS=True
-            # error
-            elif('ERROR' in data_str):
-                self.readyToSMS=True
-            #elif('AT+' not in data_str):
-                #sendSMSAdmin("I don't understand \n" + data_str)
+        if ('STATUS' in data_str):
+            self.sendSMS(contact.numbers[0].number, self.checkStatus())
+        elif ('ALARMOFF' in data_str):
+            self.sendSMS(contact.numbers[0].number, "Wylaczono ALARM")
+            self.removeUserFromActive(contact.name, contact.surname)
+        elif ('ALARMON' in data_str):
+            self.sendSMS(contact.numbers[0].number, "Wlaczono ALARM")
+            self.addUserAsActive(contact.name, contact.surname)
+        else:
+            self.sendSMS(contact.numbers[0].number,
+                         "Nie wiem, czego ode mnie zadasz")
 
 
-#if __name__ == "__main__":
+# if __name__ == "__main__":
 #    notificationManager = NotificationManager()
 #    notificationManager.sendMailNotification("IPRecorder","Hello World")
--- MonitoringManager/MonitoringManager/StatusManager.py
+++ MonitoringManager/MonitoringManager/StatusManager.py
@@ -76,60 +76,6 @@ class IpRecorderStatus:
         else:
             return info
 
-class GSMSerial:
-    #serialGSM = serial.Serial('/dev/virtualcom0',19200)  # open serial port   
-    def __init__(self):
-        self.serialGSM = serial.Serial('/dev/ttyAMA0',19200)  # open serial port   
-        commend1='AT\r\n'
-        self.serialGSM.write(commend1.encode())
-        while self.serialGSM.inWaiting() <= 1:
-            time.sleep(0.01)
-        self.serialGSM.read(self.serialGSM.inWaiting())
-        time.sleep(0.5)
-
-        commend1='AT+CMGF=1\r\n'
-        self.serialGSM.write(commend1.encode())
-        while self.serialGSM.inWaiting() <= 1:
-            time.sleep(0.01)
-        self.serialGSM.read(self.serialGSM.inWaiting())
-        time.sleep(0.5)
-
-        commend2='AT+CNMI=1,2,0,0\r\n'
-        self.serialGSM.write(commend2.encode())
-        while self.serialGSM.inWaiting()<=1:
-            time.sleep(0.01)
-        self.serialGSM.read(self.serialGSM.inWaiting())
-        time.sleep(0.5)
-
-        commend3='AT+CLIP=1\r\n'
-        self.serialGSM.write(commend3.encode())
-        while self.serialGSM.inWaiting()<=1:
-            time.sleep(0.01)
-        time.sleep(0.5)
-
-    def sendSMS(self, number, text):
-        commend='AT+CMGS="' + str(number) + '"\r\n'
-        Logger.INFO("sending SMS to " + str(number))
-        self.serialGSM.write(commend.encode())
-        while self.serialGSM.inWaiting()<=1:
-            time.sleep(0.04)
-        time.sleep(0.2)
-        data_str = self.serialGSM.read(self.serialGSM.inWaiting())
-        data_str = data_str.decode("utf-8")
-        if ('>' in data_str):
-            self.serialGSM.write(text.encode()) #\x1a
-            self.serialGSM.write(bytes(b'\x1a'))
-
-    def readMessages(self):
-        if (self.serialGSM.inWaiting()>0): #if incoming bytes are waiting to be read from the serial input buffer
-            # NB: for PySerial v3.0 or later, use property `in_waiting` instead of function `inWaiting()` below!
-            #data_str = ser.read(ser.inWaiting()).decode('ascii') #read the bytes and convert from binary array to ASCII
-            data_str = self.serialGSM.read(self.serialGSM.inWaiting())
-            data_str = data_str.decode("utf-8")
-            Logger.INFO(data_str)
-            return data_str
-        return None    
-
 if __name__ == '__main__':
     print("GSMSerial")
     status = IpRecorderStatus()
